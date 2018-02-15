250607f5d34dbba97c462e2eeb9b2a7e52d745f6
ss093182ss <b05902031@linux12.csie.ntu.edu.tw>
2018-02-15 21:30:10 +0800

    almost done

--- client.py
+++ client.py
@@ -1,10 +1,50 @@
 import socket
+import json
+import select
+import sys
+def receive(fd):
+    s = fd.recv(6000)
+    while 1:
+        if 10 not in s:
+            s = s + fd.recv(6000)
+        else:
+            break
+    return json.loads(s.decode('utf8'))
+
+def receive_from_input():
+    if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+        s = sys.stdin.readline()
+        d = {'cmd':'text','content':s[:len(s)-1]}
+        return d
+    else:
+        return None
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    sock.connect(('140.112.30.43',44444))
+    sock.connect(('140.112.30.43',44445))
 except:
     sock.close()
     print('socket fail.')
     exit()
-
+name_dic = {'cmd':'matching','name':'James','target':'Isabella'}
+sock.send((json.dumps(name_dic)+'\n').encode('utf8'))
+recv_dict = receive(sock)
+sock.settimeout(0.1)
+print(recv_dict)
+while True:
+    s_dict = receive_from_input()
+    if s_dict:
+        sock.send((json.dumps(s_dict)+'\n').encode('utf8'))
+    get = None
+    try:
+        get = sock.recv(6000)
+    except:
+        a = 0
+    if get:
+        get_dict = json.loads(get.decode('utf8'))
+        print(get_dict)
+        if get_dict['cmd'] == 'error':
+            print(get_dict['message'])
+            break
+        print(json.loads(get.decode('utf8'))['content'])
 sock.close()
--- onomat_server.py
+++ onomat_server.py
@@ -4,6 +4,7 @@ import json
 import select
 import sys
 import time
+import copy
 def check_argument():
     if len(sys.argv) != 2:
         print('You need to assign port.')
@@ -25,12 +26,47 @@ def new_client(sock_fd):
     print('new connection from',address)
     #print(client_socket.fileno())
     return client_socket
+def parse_matchlist():
+    f = open('match_list','r')
+    l = f.read().split('\n')
+    d = {}
+    for i in l:
+        if i == '':
+            break
+        name = i.split(' ')
+        d[name[0]] = name[1]
+        d[name[1]] = name[0]
+    f.close()
+    return d
+def receive(fd):
+    s = fd.recv(6000)
+    if s.decode('utf8') == '':
+        return None
+    while 1:
+        if 10 not in s:
+            s = s + fd.recv(6000)
+        elif s.decode('utf8') == '':
+            return None
+        else:
+            break
+    return json.loads(s.decode('utf8'))
+
+def check_matching(get,match):
+    try:
+        if match[get['name']] == get['target']:
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
 def select_start(sock_fd):
     print('server starts.')
     print('waiting for connecting......')
-    user_data = [{'status':0,'name':None,'match_name':None} for i in range(200)]
+    match = parse_matchlist()
+    user_data = [{'status':0,'name':None,'match_name':None,'match_fd':None} for i in range(200)]
     fd_list = [sock_fd,sys.stdin]
+    print(match)
     while True:
         readable, writable, errored = select.select(fd_list, [], [])
         for fd in readable:
@@ -43,16 +79,45 @@ def select_start(sock_fd):
                 if s == "quit\n":
                     return 0
             else:
-                s = fd.recv(4096).decode('utf8')
-                if s == '':
+                get = receive(fd)
+                if get == None:
                     fd_list.remove(fd)
                     print('------close------')
                     print('fd :',fd.fileno())
                     print(user_data[fd.fileno()])
                     print('------')
-                    user_data[fd.fileno()] = {'status':0,'name':None,'match_name':None}
+                    if user_data[fd.fileno()]['status'] == 2:
+                        print('send quit')
+                        user_data[user_data[fd.fileno()]['match_fd'].fileno()]['status'] = 1
+                        err = {'cmd':'error','message':'other side quit'}
+                        user_data[fd.fileno()]['match_fd'].send((json.dumps(err)+'\n').encode('utf8'))
+                        user_data[user_data[fd.fileno()]['match_fd'].fileno()]['match_fd'] = None
+                    user_data[fd.fileno()] = {'status':0,'name':None,'match_name':None,'match_fd':None}
                     fd.close()
-
+                elif get['cmd'] == 'matching':
+                    if check_matching(get,match) == 0:
+                        continue
+                    match_fd = [item for item in range(len(user_data)) if user_data[item]['name'] == get['target']]
+                    if len(match_fd) == 0:
+                        user_data[fd.fileno()] = {'status':1,'name':copy.copy(get['name']),'match_name':copy.copy(get['target']),'match_fd':0}
+                        print(get['name'],'waiting for',get['target'])
+                    else:
+                        for item in fd_list:
+                            if item.fileno() == match_fd[0]:
+                                user_data[fd.fileno()] = {'status':2,'name':copy.copy(get['name']),'match_name':copy.copy(get['target']),'match_fd':item}
+                                user_data[match_fd[0]]['status'] = 2
+                                user_data[match_fd[0]]['match_fd'] = fd
+                                print(get['name'],'match',get['target'])
+                                message = {'cmd':'matched'}
+                                fd.send((json.dumps(message)+'\n').encode('utf8'))
+                                item.send((json.dumps(message)+'\n').encode('utf8'))
+                                break
+                elif get['cmd'] == 'text':
+                    if user_data[fd.fileno()]['status'] == 1:
+                        err = {'cmd':'error','message':'You have not matched yet.'}
+                        fd.send((json.dumps(err)+'\n').encode('utf8'))
+                    else:
+                        user_data[fd.fileno()]['match_fd'].send((json.dumps(get)+'\n').encode('utf8'))
 
 if __name__ == "__main__":
     check_argument()
